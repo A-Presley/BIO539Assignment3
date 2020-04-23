80ee90b2b5c27947a2afc3593ba3c1d6d897e780
Jordan Lally <jordan_lally@uri.edu>
2020-04-23 16:29:04 -0400

    Here is my change for challenge 3. Need to submit a pull request

--- data_analysis.sh
+++ data_analysis.sh
@@ -3,5 +3,6 @@
 
 #This is Andrew making change 1
 #Change1
-
+#Hello! This is Jordan (1) making a change to Andrew's (2) version of the repo
+#I will then submit a pull request so you can add these changes
 
